956938369e64a4d3bf432cf72a0d5a00e4571923
MortallicaXxX <MortallicaXxX@hotmail.com>
2024-05-21 17:38:30 +0000

    Add generated files

---
+++ webComponent/index.d.ts
@@ -0,0 +1,4 @@
+import { FASTElement, customElement, css, html, attr, Observable, observable, ViewTemplate, when, repeat, ref, children, slotted, volatile } from "@microsoft/fast-element";
+export declare class WebComponent extends FASTElement {
+}
+export { customElement, css, html, attr, Observable, observable, ViewTemplate, when, repeat, ref, children, slotted, volatile };

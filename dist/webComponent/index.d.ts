279f24042cd18868b6d2ddfb401f88c02770cce1
Guillaume Houthoofd <47045327+MortallicaXxX@users.noreply.github.com>
2024-05-25 15:06:51 +0200

    update

--- dist/webComponent/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { FASTElement, customElement, css, html, attr, Observable, observable, ViewTemplate, when, repeat, ref, children, slotted, volatile } from "@microsoft/fast-element";
-export declare class WebComponent extends FASTElement {
-}
-export { customElement, css, html, attr, Observable, observable, ViewTemplate, when, repeat, ref, children, slotted, volatile };

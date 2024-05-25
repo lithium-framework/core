b3100d284e9a55cce17843c656166cb8a81b5ecd
Guillaume Houthoofd <47045327+MortallicaXxX@users.noreply.github.com>
2024-05-25 15:08:04 +0200

    update

---
+++ dist/webComponent/index.js
@@ -0,0 +1,5 @@
+export { customElement, css, html, Observable, ViewTemplate, volatile } from "@microsoft/fast-element";
+export { WebComponent } from './model.js';
+export { attr, observable, state, attrState, } from './decorators/index.js';
+export { when, repeat, ref, children, slotted } from './directives/index.js';
+//# sourceMappingURL=index.js.map

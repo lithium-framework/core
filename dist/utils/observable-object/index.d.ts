70165fb0564c2e383d6447ef717393be2814b606
MortallicaXxX <MortallicaXxX@hotmail.com>
2024-05-31 17:19:11 +0000

    Clean dist folder

--- dist/utils/observable-object/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ObservableObject } from './models.js';
-export declare function createObservableObject<Key extends string, Value extends any>(initialObject: Record<Key, Value>): ObservableObject<Key, Value>;
-export { ObservableObject };

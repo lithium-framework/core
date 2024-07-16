57e84681f829df546b471d29533afcc3c9bfbad8
MortallicaXxX <MortallicaXxX@hotmail.com>
2024-07-16 17:37:44 +0000

    Add generated files

--- src/utils/observable-object/index.ts
+++
@@ -1,7 +0,0 @@
-import { ObservableObject } from './models.js';
-
-export function createObservableObject< Key extends string , Value extends any >( initialObject:Record< Key , Value > ){
-  return ObservableObject.init< Key , Value >( initialObject );
-}
-
-export { ObservableObject };
